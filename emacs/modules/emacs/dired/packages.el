4e09bbe023621d935272e59092873c8baf75450b
TesseractPi <sam.g.rohrbach@proton.me>
2025-04-14 14:21:23 -0700

    emacs

---
+++ emacs/modules/emacs/dired/packages.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; -*-
+;;; emacs/dired/packages.el
+
+(package! dirvish
+  :recipe (:host github :repo "alexluigit/dirvish")
+  :pin "2d63995d3296c259cef90830cf0433602429ecec")
+(package! diredfl :pin "fe72d2e42ee18bf6228bba9d7086de4098f18a70")
